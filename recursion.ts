cff7b8a1fe9721ea8041a377046b49fb101568be
Khaerunnisa <nisa@js.local>
2020-08-12 18:49:04 +0800

    Added OddValues

--- recursion.ts
+++ recursion.ts
@@ -29,3 +29,44 @@ const factorial = (num: number): number | string => {
 };
 
 //console.log(factorial(4));
+
+// 3. Write a function that accepts array of number and take all the odd value.
+
+// [1,2,3,4,5] return [1,3,5]
+// [] return []
+
+// Solution with the helper function
+const oddValues = (arr: number[]): number[] => {
+	let result = [];
+
+	const helperFunction = (helperArrInput: number[]): number[] => {
+		if (!Array.isArray(helperArrInput) || helperArrInput.length === 0) return;
+
+		if (helperArrInput[0] % 2 !== 0) {
+			result.push(helperArrInput[0]);
+		}
+
+		helperFunction(helperArrInput.slice(1));
+	};
+
+	helperFunction(arr);
+	return result;
+};
+
+//console.log(oddValues([]));
+
+// Solution with recursion
+const oddNumbers = (arr: number[]): number[] => {
+	let newArr = [];
+
+	if (!Array.isArray(arr) || arr.length === 0) return newArr;
+
+	if (arr[0] % 2 !== 0) {
+		newArr.push(arr[0]);
+	}
+
+	newArr = newArr.concat(oddNumbers(arr.slice(1)));
+	return newArr;
+};
+
+console.log(oddNumbers([1, 2, 3, 4, 5]));
