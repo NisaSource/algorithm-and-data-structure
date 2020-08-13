c5a5ca49c8adf654146d9aa2896107d8a46b3916
Khaerunnisa <nisa@js.local>
2020-08-13 21:22:55 +0800

    Added insertionSort

--- sortAlgorithm.ts
+++ sortAlgorithm.ts
@@ -48,5 +48,24 @@ const selectionSort = (arr: number[] | string[]): number[] | string[] => {
 	return arr;
 };
 
-console.log(selectionSort([3, 2, 5, 4, 1]));
-console.log(selectionSort(['h', 'a', 'g', 'e', 'd']));
+// console.log(selectionSort([3, 2, 5, 4, 1]));
+// console.log(selectionSort(['h', 'a', 'g', 'e', 'd']));
+
+// 3. Insertion sort
+// inserting one item at a time in a right place
+
+const insertionSort = (arr: number[] | string[]): number[] | string[] => {
+	for (let i = 1; i < arr.length; i++) {
+		let current = arr[i];
+		let j = i - 1;
+		while (j >= 0 && arr[j] > current) {
+			arr[j + 1] = arr[j];
+			j -= 1;
+		}
+		arr[j + 1] = current;
+	}
+	return arr;
+};
+
+// console.log(insertionSort([3, 2, 5, 4, 1]));
+// console.log(insertionSort(['c', 'a', 'b', 'e', 'd']));
