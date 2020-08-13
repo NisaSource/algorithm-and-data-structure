9a60fee8ed9eee9e6a9f6c9132ddf6eefb29062d
Khaerunnisa <nisa@js.local>
2020-08-13 15:17:38 +0800

    Added binarySearch

--- searchAlgorithm.ts
+++ searchAlgorithm.ts
@@ -18,5 +18,35 @@ const linearSearch = (
 	return -1;
 };
 
-console.log(linearSearch([1, 2, 3, 4, 5, 6], 6));
-console.log(linearSearch([1, 2, 3, 4, 5, 6], 100));
+// console.log(linearSearch([1, 2, 3, 4, 5, 6], 6));
+// console.log(linearSearch([1, 2, 3, 4, 5, 6], 100));
+
+// 2. Write a function which accepts a sorted array and a value,
+// and return the index at which the value exist. Otherwise, return -1.
+
+// binarySearch([1,2,3,4,5, 6], 3) return 2
+// binarySearch([1,2,3,4,5, 6], 25) return -1
+
+const binarySearch = (
+	arr: number[] | string[],
+	val: number | string
+): number => {
+	if (!Array.isArray(arr) || arr.length < 1) return null;
+
+	let startPoint = 0;
+	let endPoint = arr.length - 1;
+	let midPoint = Math.floor((startPoint + endPoint) / 2);
+
+	while (arr[midPoint] !== val && startPoint < endPoint) {
+		if (val < arr[midPoint]) {
+			endPoint = midPoint - 1;
+		} else {
+			startPoint = midPoint + 1;
+		}
+		midPoint = Math.floor((startPoint + endPoint) / 2);
+	}
+	return arr[midPoint] === val ? midPoint : -1;
+};
+
+// console.log(binarySearch([1, 2, 3, 4, 5, 6], 3));
+// console.log(binarySearch([1, 2, 3, 4, 5, 6], 25));
